14f3a8154d130b7aef701c2996765c33baf70d44
cdj-xh <jingsulong@gmail.com>
2022-04-13 18:39:57 +0800

    feat(测试项目打包结果):

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello,world');
+class CdjCache {
+  constructor() {
+    this.cacheData = {};
+  }
+
+  setItem(key, value) {
+    this.cacheData[key] = value;
+  }
+
+  getItem(key) {
+    return this.cacheData[key]
+  }
+}
+
+window.CdjCache = CdjCache;
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
+        test: /\.js$/,
         loader: 'babel-loader'
       }
     ]
